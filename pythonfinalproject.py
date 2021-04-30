0b646b466d1fe9dede10a8869b80db7bc2c0b47d
Erdem <64652384+ErdemGurel@users.noreply.github.com>
2021-04-30 17:12:02 +0300

    ne oluyo mk

--- pythonfinalproject.py
+++ pythonfinalproject.py
@@ -1,1 +1,8 @@
-print(("aa"))
+def main():
+    balance = int(input("How much money you want to invest: "))
+    bet = int(input("How much you want to bet: "))
+    balance -= bet
+    print("Your new ballance: {}".format(balance))
+    
+
+    
