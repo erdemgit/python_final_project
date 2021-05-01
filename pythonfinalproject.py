3e3376b23b7ef3225b8034d2d530aadf6a77fa6e
Erdem <64652384+ErdemGurel@users.noreply.github.com>
2021-05-01 17:35:08 +0300

    basardik, tebrik ederim

--- pythonfinalproject.py
+++ pythonfinalproject.py
@@ -8,12 +8,69 @@ def main():
         }
     cards = "A234567891JQK"
     bet = takethebet()
+    
     player_hand = firsthand(cards, dest)
     if isitblackjack(player_hand): playerwins(bet) 
+    print("Player Hand: {}".format(player_hand))
+    
     case_hand = firsthand(cards, dest)
     if isitblackjack(case_hand): casewins(bet)
-    print("{}".format(case_hand[0]))
+    print("Case Hand: {}".format(case_hand[0]))
+    
+    while doyouwanttodraw():
+        player_hand += drawcard(cards, dest)
+        print("Player cards: {}".format(player_hand))
+        if isitover21(player_hand, dest): casewins(bet)
+    
+    print("Case cards: {}".format(case_hand))
+    while shouldcasedrawcard(player_hand, case_hand, dest):
+        case_hand += drawcard(cards, dest)
+        print("Case cards: {}".format(case_hand))
+        if isitover21(case_hand, dest): playerwins(bet)
+    
+    print("Player cards: {}\nCase cards: {}".format(player_hand, case_hand))
+    whowins(player_hand, case_hand, dest, bet)
+    
+def whowins(player_hand, case_hand, dest, bet): 
+    player_score = handscore(player_hand, dest)
+    case_score = handscore(case_hand, dest)
+    if player_score > case_score: playerwins(bet)
+    elif player_score == case_score: tie(bet)
+    else: casewins(bet)
+
+def tie(bet):
+    return print("Tie, you won {}".format(bet))
     exit()
+
+def shouldcasedrawcard(player_hand, case_hand, dest):
+    if handscore(case_hand, dest) < 17: return True
+    return False
+
+def isitover21(hand, dest): #A  1 or 11
+    if handscore(hand, dest) > 21:
+        return True
+    return False
+
+def handscore(hand, dest):
+    score = 0
+    for card in hand:
+        score += dest[card][0]
+    if score > 21 and "A" in hand: score -= 10
+    return score
+
+def drawcard(cards, dest):
+    card = choice(cards)
+    if dest[card][1] > 0:
+        dest[card][1] -= 1
+        return card
+    else: 
+       return drawcard(hand, cards, dest)
+    
+def doyouwanttodraw():
+    task = input("Do you want to draw a card.")
+    if task.lower()[0] == "y":
+        return True
+    return False
 
 def casewins(bet):
     print("Sorry, you just lost: {}".format(bet))
